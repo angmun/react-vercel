43fba17b1b841135bcc670ff1f031dd680753b25
Angelica Munyao <munyaoangelica@yahoo.com>
2020-09-17 23:51:58 -0400

    Added environment variable

--- src/data/api/events.js
+++ src/data/api/events.js
@@ -1,13 +1,14 @@
-// apiKey: 'keyr1gsZzM2vaTR2k'
 export function fetchEvents(baseKey, table, { noOfRecords, tableView }) {
+  const apiKey = process.env.REACT_APP_AIRTABLE_TEST;
+  console.log(process.env);
   return fetch(
     `https://api.airtable.com/v0/${baseKey}/${table}?maxRecords=${noOfRecords}&view=${encodeURI(
       tableView
-    )}&api_key=keyr1gsZzM2vaTR2k`
+    )}&api_key=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
       return data.records;
     })
-    .catch((error) => console.error("From fetchEvents:", error));
+    .catch((error) => console.error('From fetchEvents:', error));
 }
